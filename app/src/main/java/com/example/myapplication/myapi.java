53a328c9899685a315d29750e33bd04f10186f4f
firmanadie <firmanadi.afuah@gmai.com>
2021-06-01 09:36:31 +0700

    Initial commit

---
+++ app/src/main/java/com/example/myapplication/myapi.java
@@ -0,0 +1,16 @@
+package com.example.myapplication;
+
+
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+
+public interface myapi {
+
+    @GET("posts")
+    Call<List<model>> getmodels();
+
+
+}
